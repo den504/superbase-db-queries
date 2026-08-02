7cecfd9aa6cd1087aa089ab6d7371966edcaf202
Dennis <dennisokaforab@yahoo.com>
2026-08-02 16:49:32 +0100

    feat: creator-auth-01

--- creator_inc_storage_policy.sql
+++ creator_inc_storage_policy.sql
@@ -33,3 +33,33 @@ using (
   bucket_id = 'profile-photos'
   and (storage.foldername(name))[1] = auth.uid()::text
 );
+
+
+---- storage policy for video storage bucket----------
+
+create policy "Creators can upload own shorts"
+on storage.objects for insert
+to authenticated
+with check (
+    bucket_id = 'creator-videos'
+    and (storage.foldername(name))[1] = auth.uid()::text
+);
+
+
+
+create policy "Creators can update own shorts"
+on storage.objects for update
+to authenticated
+using (
+    bucket_id = 'creator-videos'
+    and (storage.foldername(name))[1] = auth.uid()::text
+);
+
+
+create policy "Creators can view own shorts"
+on storage.objects for select
+to authenticated
+using (
+    bucket_id = 'creator-videos'
+    and (storage.foldername(name))[1] = auth.uid()::text
+);
